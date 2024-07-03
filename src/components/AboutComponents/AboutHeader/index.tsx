f5c97ea97b82e4744466a53c1d5088bde71f7bbc
terexfed <terexfed9@gmail.com>
2024-07-03 16:55:22 +0300

    image fix [UBR-230]

--- src/components/AboutComponents/AboutHeader/index.tsx
+++ src/components/AboutComponents/AboutHeader/index.tsx
@@ -41,18 +41,18 @@ export const AboutHeader: FC = () => {
 
   return (
     <div className={`container ${styles.mobileContainer}`}>
-        <div className={` ${styles.container}`}>
-          <div>
-            <h1 className={styles.header}>
-              {/* <span className={styles.blueText}>Весь рынок</span> клининга на одной площадке */}
-              О сервисе <span className={styles.blueText}>Ubrato</span>
-            </h1>
-            <p className={styles.agregator}> <span className={styles.blueText}>Ubrato</span> — площадка для заказчиков и исполнителей клининговых услуг. <span className={styles.span_marginBlock}></span>
-Мы создали <span className={styles.blueText}>профильный агрегатор</span>, чтобы рынок клининга стал чище, а поиск партнёров быстрее и легче. Чтобы быть уверенными в опыте партнёров и качестве услуг, мы работаем только с юридическими лицами.</p>
-          </div>
-          <img className={styles.image} src="./muzhik.png" alt="big-muzhik" />
-          {/* muzhik */}
+      <div className={` ${styles.container}`}>
+        <div>
+          <h1 className={styles.header}>
+            {/* <span className={styles.blueText}>Весь рынок</span> клининга на одной площадке */}
+            О сервисе <span className={styles.blueText}>Ubrato</span>
+          </h1>
+          <p className={styles.agregator}> <span className={styles.blueText}>Ubrato</span> — площадка для заказчиков и исполнителей клининговых услуг. <span className={styles.span_marginBlock}></span>
+            Мы создали <span className={styles.blueText}>профильный агрегатор</span>, чтобы рынок клининга стал чище, а поиск партнёров быстрее и легче. Чтобы быть уверенными в опыте партнёров и качестве услуг, мы работаем только с юридическими лицами.</p>
         </div>
+        <img className={styles.image} src="./muzhik.png" alt="big-muzhik" />
+        {/* muzhik */}
+      </div>
     </div>
   );
 };
