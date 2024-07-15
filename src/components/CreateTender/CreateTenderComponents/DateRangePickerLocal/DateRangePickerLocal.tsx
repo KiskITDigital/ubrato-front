49d180552aa556453d7582126153ec464c6298b9
Arsen4ik <arsenfazleev@gmail.com>
2024-07-15 22:58:08 +0300

    some updates are done

--- src/components/CreateTender/CreateTenderComponents/DateRangePickerLocal/DateRangePickerLocal.tsx
+++ src/components/CreateTender/CreateTenderComponents/DateRangePickerLocal/DateRangePickerLocal.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect } from "react";
 import { DateRangePicker } from "@nextui-org/date-picker";
 import styles from './DateRangePickerLocal.module.css'
 import { RangeValue, CalendarDate } from "@nextui-org/react";
@@ -8,8 +8,10 @@ import { getLocalTimeZone, parseDate, today } from "@internationalized/date";
 const DateRangePickerLocal: FC<{
     timeToChangeStart: 'reception_start' | 'work_start',
     timeToChangeEnd: 'reception_end' | 'work_end',
-}> = ({ timeToChangeStart, timeToChangeEnd }) => {
-    const createTenderState = useCreateTenderState()
+    state: boolean,
+    setState: (newVal: boolean) => void
+}> = ({ timeToChangeStart, timeToChangeEnd, state, setState }) => {
+    const createTenderState = useCreateTenderState();
 
     const classNames = {
         base: styles["base"],
@@ -35,33 +37,36 @@ const DateRangePickerLocal: FC<{
         gridBodyRow: styles["grid-body-row"],
         gridBody: styles["grid-body"],
         header: styles["header"]
-    }
-    const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false);
+    };
 
     useEffect(() => {
         if (createTenderState.work_start.getTime() < createTenderState.reception_end.getTime()) {
-            createTenderState.handleSimpleInput('work_start', createTenderState.reception_end)
-            if (createTenderState.work_end.getTime() < createTenderState.reception_end.getTime()) createTenderState.handleSimpleInput('work_end', createTenderState.reception_end)
+            createTenderState.handleSimpleInput('work_start', createTenderState.reception_end);
+            if (createTenderState.work_end.getTime() < createTenderState.reception_end.getTime()) {
+                createTenderState.handleSimpleInput('work_end', createTenderState.reception_end);
+            }
         }
     }, [createTenderState, createTenderState.reception_end]);
 
     return (
         <DateRangePicker
+            aria-label="calendar"
             minValue={timeToChangeStart === 'work_start' ? parseDate(createTenderState.reception_end.toISOString().split('T')[0]) : today(getLocalTimeZone())}
             maxValue={parseDate("2030-01-01")}
             labelPlacement="outside-left"
-            onFocus={() => setIsCalendarOpen(true)}
-            onOpenChange={() => setIsCalendarOpen(prev => !prev)}
-            isOpen={isCalendarOpen}
+            isOpen={state}
             value={{
                 start: parseDate(createTenderState[timeToChangeStart].toISOString().split('T')[0]),
                 end: parseDate(createTenderState[timeToChangeEnd].toISOString().split('T')[0])
             }}
             onChange={(newVal: RangeValue<CalendarDate>) => {
-                const firstDate = new Date(newVal.start.year, newVal.start.month - 1, newVal.start.day, 3)
-                const secondDate = new Date(newVal.end.year, newVal.end.month - 1, newVal.end.day, 3)
+                setState(false)
+
+                const firstDate = new Date(newVal.start.year, newVal.start.month - 1, newVal.start.day, 3);
+                const secondDate = new Date(newVal.end.year, newVal.end.month - 1, newVal.end.day, 3);
                 if (firstDate.getFullYear() > 2030 || firstDate.getFullYear() < new Date().getFullYear() || secondDate.getFullYear() > 2030 || secondDate.getFullYear() < new Date().getFullYear()) return;
-                createTenderState.handleSimpleInput(timeToChangeStart, firstDate); createTenderState.handleSimpleInput(timeToChangeEnd, secondDate)
+                createTenderState.handleSimpleInput(timeToChangeStart, firstDate);
+                createTenderState.handleSimpleInput(timeToChangeEnd, secondDate);
             }}
             classNames={classNames}
         />
--- src/components/CreateTender/CreateTenderComponents/Dates/Dates.tsx
+++ src/components/CreateTender/CreateTenderComponents/Dates/Dates.tsx
@@ -1,5 +1,151 @@
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { FC, forwardRef, useEffect, useRef, useState } from "react";
+// import styles from '../../CreateTender.module.css'
+// import datesStyles from './Dates.module.css'
+// import { useCreateTenderState } from "@/store/createTenderStore";
+// import { Checkbox, Switch } from "@nextui-org/react";
+// import {
+//     addTwoDots,
+//     checkOnlyNumber
+// } from "../../funcs";
+// import DateRangePickerLocal from "../DateRangePickerLocal/DateRangePickerLocal";
+
+// const Dates: FC<{ ref?: React.LegacyRef<HTMLDivElement>; }> = forwardRef<HTMLDivElement, { ref?: React.LegacyRef<HTMLDivElement>; }>((_, ref) => {
+//     const createTenderState = useCreateTenderState()
+
+//     const calendarRef1 = useRef<HTMLDivElement>(null)
+//     const calendarRef2 = useRef<HTMLDivElement>(null)
+
+//     const spanRef = useRef<HTMLSpanElement>(null)
+
+//     const priceInputRef1 = useRef<HTMLInputElement>(null)
+//     const priceInputRef2 = useRef<HTMLInputElement>(null)
+
+//     const [calendar1, setCalendar1] = useState(false);
+//     const [calendar2, setCalendar2] = useState(false);
+
+//     useEffect(() => {
+//         const handleClickOutside = (event: MouseEvent) => {
+//             const target = event.target;
+//             if (target instanceof Node) {
+//                 if (calendarRef1.current && !calendarRef1.current.contains(target)) {
+//                     setCalendar1(false);
+//                     spanRef.current?.focus();
+//                 }
+//                 if (calendarRef2.current && !calendarRef2.current.contains(target)) {
+//                     setCalendar2(false);
+//                     spanRef.current?.focus();
+//                 }
+//             }
+//         };
+
+//         window.addEventListener("click", handleClickOutside);
+
+//         return () => {
+//             window.removeEventListener("click", handleClickOutside);
+//         };
+//     }, []);
+
+
+//     return (
+//         <div ref={ref} className={`${styles.firstSections}`}>
+//             <span ref={spanRef} tabIndex={-1}></span>
+//             <div className={`${styles.firstSections__div} ${styles.firstSections__responses}`}>
+//                 <div className={`${styles.firstSections__div__title}`}>
+//                     <p className={`${styles.firstSections__div__title__p}`}>Прием откликов</p>
+//                 </div>
+//                 <div ref={calendarRef1} className={`${styles.firstSections__div__main} ${styles.firstSections__div__mainWork} ${datesStyles.dateTimeContainer}`} onFocus={(event) => {
+//                     const target = event.target
+//                     if (target instanceof Node) {
+//                         if (!priceInputRef1.current?.contains(target)) {
+//                             setCalendar1(true)
+//                         } else {
+//                             priceInputRef1.current.focus()
+//                         }
+//                     }
+//                 }}
+//                 >
+//                     <p className={`${styles.firstSections__div__main__block__p}`}>Начало</p>
+//                     <p className={`${styles.firstSections__div__main__block__p}`}>Окончание</p>
+//                     <DateRangePickerLocal state={calendar1} setState={(newVal: boolean) => { setCalendar1(newVal); spanRef.current?.focus() }} timeToChangeStart="reception_start" timeToChangeEnd="reception_end" />
+//                     <div className={datesStyles.dateTime}>
+//                         <span className={styles.firstSections__responses__inputs__span}></span>
+//                         <input
+//                             ref={priceInputRef1}
+//                             placeholder="00:00"
+//                             maxLength={5}
+//                             value={createTenderState.reception_time_start}
+//                             onChange={(e) => createTenderState.handleSimpleInput('reception_time_start', e.currentTarget.value, addTwoDots)}
+//                             type="text" className={`${styles.input} ${styles.firstSections__responses__inputs__input2}`} />
+//                     </div>
+//                     <div className={`${datesStyles.dateTime} ${datesStyles.dateTime2}`}>
+//                         <span className={styles.firstSections__responses__inputs__span}></span>
+//                         <input
+//                             ref={priceInputRef2}
+//                             placeholder="00:00"
+//                             maxLength={5}
+//                             value={createTenderState.reception_time_end}
+//                             onChange={(e) => createTenderState.handleSimpleInput('reception_time_end', e.currentTarget.value, addTwoDots)}
+//                             type="text" className={`${styles.input} ${styles.firstSections__responses__inputs__input2}`} />
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className={`${styles.firstSections__div}`}>
+//                 <div className={`${styles.firstSections__div__title}`}>
+//                     <p className={`${styles.firstSections__div__title__p}`}>Стоимость</p>
+//                     <Switch aria-hidden={false} className={`${styles.SwitchNextUI} ${createTenderState.is_NDS ? styles.SwitchNextUIOn : ''}`} isSelected={createTenderState.is_NDS} onValueChange={() => createTenderState.handleSimpleInput('is_NDS', !createTenderState.is_NDS)}>
+//                         вкл. НДС
+//                     </Switch>
+//                 </div>
+//                 <div className={`${styles.firstSections__div__main}`}>
+//                     <div className={`${styles.firstSections__div__main__block}`}>
+//                         <p className={`${styles.firstSections__div__main__block__p}`}>Стоимость в рублях</p>
+//                         <input
+//                             onFocus={() => createTenderState.removeError('price')}
+//                             onBlur={() => { !+createTenderState.price && createTenderState.addError('price'); +createTenderState.price === 0 && createTenderState.handleSimpleInput("price", "") }}
+//                             value={createTenderState.price}
+//                             onChange={(e) => createTenderState.handleSimpleInput('price', e.currentTarget.value, checkOnlyNumber)}
+//                             className={`${styles.input} ${styles.firstSections__div__main__block__input} ${createTenderState.errors.includes('price') && !createTenderState.is_contract_price ? styles.inputError : ''}`}
+//                             type="text" />
+//                         {createTenderState.errors.includes('price') && !createTenderState.is_contract_price && <p className={`${styles.inputErrorText} ${styles.inputErrorTextPrice} ${styles.inputErrorTenderPrice}`}>Обязательно для заполнения</p>}
+//                     </div>
+//                     <div className={`${styles.firstSections__div__main__block} ${styles.CheckboxNextUI__block}`}>
+//                         <span className={`${styles.CheckboxNextUI__block__span}`}></span>
+//                         <Checkbox onValueChange={() => createTenderState.handleSimpleInput('is_contract_price', !createTenderState.is_contract_price)} className={`${styles.CheckboxNextUI} ${createTenderState.is_contract_price ? styles.CheckboxNextUIActive : ''} ${styles.CheckboxNextUIIsContract}`} isSelected={createTenderState.is_contract_price}>
+//                             Договорная
+//                         </Checkbox>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className={`${styles.firstSections__div} ${styles.firstSections__responses}`}>
+//                 <div className={`${styles.firstSections__div__title}`}>
+//                     <p className={`${styles.firstSections__div__title__p}`}>Оказание улуг</p>
+//                 </div>
+//                 <div className={`${styles.firstSections__div__main} ${styles.firstSections__div__mainWork}`} ref={calendarRef2} onFocus={() => setCalendar2(true)}>
+//                     <p className={`${styles.firstSections__div__main__block__p}`}>Начало</p>
+//                     <p className={`${styles.firstSections__div__main__block__p}`}>Окончание</p>
+//                     <DateRangePickerLocal state={calendar2} setState={(newVal: boolean) => { setCalendar2(newVal); spanRef.current?.focus() }} timeToChangeStart="work_start" timeToChangeEnd="work_end" />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// })
+
+// export default Dates;
+
+
+
+
+
+
+
+
+
+
+
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { FC, forwardRef } from "react";
+import { FC, forwardRef, useEffect, useRef, useState } from "react";
 import styles from '../../CreateTender.module.css'
 import datesStyles from './Dates.module.css'
 import { useCreateTenderState } from "@/store/createTenderStore";
@@ -13,19 +159,69 @@ import DateRangePickerLocal from "../DateRangePickerLocal/DateRangePickerLocal";
 const Dates: FC<{ ref?: React.LegacyRef<HTMLDivElement>; }> = forwardRef<HTMLDivElement, { ref?: React.LegacyRef<HTMLDivElement>; }>((_, ref) => {
     const createTenderState = useCreateTenderState()
 
+    const calendarRef1 = useRef<HTMLDivElement>(null)
+    const calendarRef2 = useRef<HTMLDivElement>(null)
+
+    const spanRef = useRef<HTMLSpanElement>(null)
+
+    const priceInputRef1 = useRef<HTMLInputElement>(null)
+    const priceInputRef2 = useRef<HTMLInputElement>(null)
+
+    const [calendar1, setCalendar1] = useState(false);
+    const [calendar2, setCalendar2] = useState(false);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            const target = event.target;
+            if (target instanceof Node) {
+                if (calendarRef1.current && !calendarRef1.current.contains(target)) {
+                    // calendarRef2.current && calendarRef2.current.contains(target) && calendarRef2.current.focus()
+                    setCalendar1(false);
+                    spanRef.current?.focus();
+                }
+                if (calendarRef2.current && !calendarRef2.current.contains(target)) {
+                    // calendarRef1.current && calendarRef1.current.contains(target) && calendarRef1.current.focus()
+                    setCalendar2(false);
+                    spanRef.current?.focus();
+                }
+            }
+        };
+
+        window.addEventListener("click", handleClickOutside);
+
+        return () => {
+            window.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
+
     return (
         <div ref={ref} className={`${styles.firstSections}`}>
+            <span ref={spanRef} tabIndex={-1}></span>
             <div className={`${styles.firstSections__div} ${styles.firstSections__responses}`}>
                 <div className={`${styles.firstSections__div__title}`}>
                     <p className={`${styles.firstSections__div__title__p}`}>Прием откликов</p>
                 </div>
-                <div className={`${styles.firstSections__div__main} ${styles.firstSections__div__mainWork} ${datesStyles.dateTimeContainer}`}>
+                <div ref={calendarRef1} className={`${styles.firstSections__div__main} ${styles.firstSections__div__mainWork} ${datesStyles.dateTimeContainer}`} onFocus={(event) => {
+                    const target = event.target;
+                    if (target instanceof Node) {
+                        if (priceInputRef1.current?.contains(target)) {
+                            priceInputRef1.current.focus();
+                        } else if (priceInputRef2.current?.contains(target)) {
+                            priceInputRef2.current.focus();
+                        } else {
+                            setCalendar1(true);
+                        }
+                    }
+                }}>
                     <p className={`${styles.firstSections__div__main__block__p}`}>Начало</p>
                     <p className={`${styles.firstSections__div__main__block__p}`}>Окончание</p>
-                    <DateRangePickerLocal timeToChangeStart="reception_start" timeToChangeEnd="reception_end" />
+                    <DateRangePickerLocal state={calendar1} setState={(newVal: boolean) => { setCalendar1(newVal); spanRef.current?.focus() }} timeToChangeStart="reception_start" timeToChangeEnd="reception_end" />
                     <div className={datesStyles.dateTime}>
                         <span className={styles.firstSections__responses__inputs__span}></span>
                         <input
+                            onClick={(e) => e.stopPropagation()}
+                            ref={priceInputRef1}
                             placeholder="00:00"
                             maxLength={5}
                             value={createTenderState.reception_time_start}
@@ -35,6 +231,8 @@ const Dates: FC<{ ref?: React.LegacyRef<HTMLDivElement>; }> = forwardRef<HTMLDiv
                     <div className={`${datesStyles.dateTime} ${datesStyles.dateTime2}`}>
                         <span className={styles.firstSections__responses__inputs__span}></span>
                         <input
+                            onClick={(e) => e.stopPropagation()}
+                            ref={priceInputRef2}
                             placeholder="00:00"
                             maxLength={5}
                             value={createTenderState.reception_time_end}
@@ -74,10 +272,15 @@ const Dates: FC<{ ref?: React.LegacyRef<HTMLDivElement>; }> = forwardRef<HTMLDiv
                 <div className={`${styles.firstSections__div__title}`}>
                     <p className={`${styles.firstSections__div__title__p}`}>Оказание улуг</p>
                 </div>
-                <div className={`${styles.firstSections__div__main} ${styles.firstSections__div__mainWork}`}>
+                <div className={`${styles.firstSections__div__main} ${styles.firstSections__div__mainWork}`} ref={calendarRef2} onFocus={(event) => {
+                    const target = event.target;
+                    if (target instanceof Node) {
+                        setCalendar2(true);
+                    }
+                }}>
                     <p className={`${styles.firstSections__div__main__block__p}`}>Начало</p>
                     <p className={`${styles.firstSections__div__main__block__p}`}>Окончание</p>
-                    <DateRangePickerLocal timeToChangeStart="work_start" timeToChangeEnd="work_end" />
+                    <DateRangePickerLocal state={calendar2} setState={(newVal: boolean) => { setCalendar2(newVal); spanRef.current?.focus() }} timeToChangeStart="work_start" timeToChangeEnd="work_end" />
                 </div>
             </div>
         </div>
