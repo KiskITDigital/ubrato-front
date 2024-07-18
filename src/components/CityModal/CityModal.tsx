e4e07e8696bdbe2cfb4b1bb358a46bb849874b37
ModerNik <oleg@detinkin.ru>
2024-07-18 14:05:46 +0300

    update: cities
    updated cities list

--- src/components/CityModal/CityModal.tsx
+++ src/components/CityModal/CityModal.tsx
@@ -2,6 +2,19 @@ import { FC, useEffect, useState } from 'react';
 import styles from './citymodal.module.css';
 import { getCities } from '@/api';
 
+const mockCities = [
+  { id: 0, name: "Москва", region: "Москва и Московская область" },
+  { id: 1, name: "Санкт-Петербург", region: "Санкт-Петербург и Ленинградская область" },
+  { id: 2, name: "Нижний Новгород", region: "Нижегородская область" },
+  { id: 3, name: "Казань", region: "Республика Татарстан" },
+  { id: 4, name: "Екатеринбург", region: "Свердловская область" },
+  { id: 5, name: "Краснодар", region: "Краснодарский край" },
+  { id: 6, name: "Самара", region: "Самарская область" },
+  { id: 7, name: "Новосибирск", region: "Новосибирская область" },
+  { id: 8, name: "Красноярск", region: "Красноярский край" },
+  { id: 9, name: "Ростов-на-Дону", region: "Ростовская область" },
+]
+
 export const CityModal: FC<{
   setConfirm: (a: boolean) => void;
   setModal: (a: boolean) => void;
@@ -14,6 +27,7 @@ export const CityModal: FC<{
 
   useEffect(() => {
     getCities("").then(response => {
+      console.log(citiesArr)
       setCitiesArr(response.data)
       setIsListOpen(true)
     })
@@ -37,23 +51,28 @@ export const CityModal: FC<{
             })();
           }}
         />
-        {citiesArr.length !== 0 && isListOpen && (
-          <div className={styles.citiesList}>
-            {citiesArr?.map((city) => (
-              <div
-                className={styles.city}
-                onClick={() => {
-                  setIsListOpen(false);
-                  setConfirmedCity(city.name);
-                  setInputValue('');
-                }}
-                key={city.id}
-              >
-                {city.name}
-                <span className={styles.region}>{city.region}</span>
+        {mockCities.length !== 0 && isListOpen && (
+          <>
+            {mockCities?.filter(filterCity => filterCity.name.toLowerCase().includes(inputValue.toLowerCase())).length ?
+              <div className={styles.citiesList}>
+                {mockCities?.filter(filterCity => filterCity.name.toLowerCase().includes(inputValue.toLowerCase()))?.map((city) => (
+                  <div
+                    className={styles.city}
+                    onClick={() => {
+                      setIsListOpen(false);
+                      setConfirmedCity(city.name);
+                      setInputValue('');
+                    }}
+                    key={city.id}
+                  >
+                    {city.name}
+                    <span className={`${styles.region} truncate`}>{city.region}</span>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
+              : <></>
+            }
+          </>
         )}
         {confirmedCity && (
           <p className={styles.newCity}>{confirmedCity}</p>
@@ -83,6 +102,6 @@ export const CityModal: FC<{
           </button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
