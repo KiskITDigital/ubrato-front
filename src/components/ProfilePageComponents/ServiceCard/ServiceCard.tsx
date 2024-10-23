0d472bd2ec31c48991923db59a68e8343eb0a35d
Pyotr Mershin <pkmershin@gmail.com>
2024-10-23 16:38:17 +0300

    contractor profile price select

--- src/components/ProfilePageComponents/ServiceCard/ServiceCard.tsx
+++ src/components/ProfilePageComponents/ServiceCard/ServiceCard.tsx
@@ -56,16 +56,76 @@ export const ServiceCard: FC<{
               console.log(e);
             }}
             classNames={{
-              mainWrapper:
-                'flex pl-[8px] justify-center bg-light-gray w-[68px] h-[30px] border-solid border-[1px] rounded-r-[6px]',
-              trigger: 'flex justify-between items-center p-0',
-              selectorIcon: 'mt-[3px] mr-[3px] z-10 h-fit relative data-[open]:rotate-180 duration-300 transition-all',
+              mainWrapper: '',
+              value: 'max-w-[60px] block',
+              trigger:
+                'p-0 gap-0 flex pl-[8px] justify-between bg-light-gray w-[88px] h-[30px] border-solid border-[1px] rounded-r-[6px] data-[open=true]:rounded-br-none',
+              selectorIcon:
+                'z-10 h-fit relative data-[open]:rotate-180 duration-300 transition-all',
               popoverContent:
-                'p-[5px] pt-[10px] ml-[-7px] mt-[-5px] w-[70px] border-solid border-accent border-[2px] border-t-0 rounded-b-[6px] bg-white',
+                'p-[5px] ml-[-22px] mt-[-5px] w-[110px] text-[12px] border-solid border-[1px] border-t-0 rounded-b-[6px] bg-white',
             }}
           >
-            <SelectItem className='rounded' key={'кв. м.'}>кв. м.</SelectItem>
-            <SelectItem key={'кв.м.'}>кв.м.</SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'ед.'}
+            >
+              ед.[единиц]
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'шт.'}
+            >
+              шт.
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'кг'}
+            >
+              кг
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'т'}
+            >
+              т[тонн]
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'м.'}
+            >
+              м.
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'кв. м.'}
+            >
+              кв. м.
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'куб. м.'}
+            >
+              куб. м.
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'ч.'}
+            >
+              ч.
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'дн.'}
+            >
+              дн.
+            </SelectItem>
+            <SelectItem
+              className="font-bold flex items-center h-[22px] rounded hover:bg-light-gray"
+              key={'мес.'}
+            >
+              мес.
+            </SelectItem>
           </Select>
         </div>
       )}
