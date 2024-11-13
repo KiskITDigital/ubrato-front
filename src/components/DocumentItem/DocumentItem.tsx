eea254265bb2780b1000bdeae34c691e758f9a2d
Pyotr Mershin <pkmershin@gmail.com>
2024-11-13 16:21:17 +0300

    documents texts

--- src/components/DocumentItem/DocumentItem.tsx
+++ src/components/DocumentItem/DocumentItem.tsx
@@ -1,25 +1,25 @@
-import { documentsContant } from '@/textData/documentsContent';
-import { Modal, ModalContent, useDisclosure } from '@nextui-org/react';
-import { FC } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { documentsContant } from "@/textData/documentsContent";
+import { Modal, ModalContent, useDisclosure } from "@nextui-org/react";
+import { FC } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export const DocumentItem: FC = () => {
   const location = useLocation();
 
-  const document = documentsContant[location.pathname.split('/')[2]];
+  const document = documentsContant[location.pathname.split("/")[2]];
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const modalStyle = {
     wrapper:
-      'z-[7] absolute top-[calc(50vh-143px)] left-[calc(50vw-270px)] w-[540px] h-[286px] bg-white p-5 shadow-md rounded-[20px]',
-    backdrop: 'z-[6] backdrop-blur absolute top-0 left-0 w-full h-full',
+      "z-[7] absolute top-[calc(50vh-143px)] left-[calc(50vw-270px)] w-[540px] h-[286px] bg-white p-5 shadow-md rounded-[20px]",
+    backdrop: "z-[6] backdrop-blur absolute top-0 left-0 w-full h-full",
     closeButton:
-      'absolute right-0 w-[34px] h-[34px] bg-[rgba(0,0,0,.04)] rounded-[10px] flex items-center justify-center',
+      "absolute right-0 w-[34px] h-[34px] bg-[rgba(0,0,0,.04)] rounded-[10px] flex items-center justify-center",
   };
 
   return (
-    <div>
+    <div className="w-full">
       {document && (
         <>
           <header className="mt-[30px] relative px-5 py-[14px] bg-[#EBF5FF] rounded-[13px]">
@@ -39,11 +39,11 @@ export const DocumentItem: FC = () => {
               <ModalContent>
                 <div className="mt-[70px] flex flex-col justify-between h-full">
                   <p className="text-lg font-semibold">
-                    Скачать документ{' '}
+                    Скачать документ{" "}
                     <span className="text-accent underline">
                       Согласие на размещение и обнародование фотографий и иных материалов для
                       Исполнителей
-                    </span>{' '}
+                    </span>{" "}
                     в формате pdf
                   </p>
                   <Link
@@ -62,7 +62,9 @@ export const DocumentItem: FC = () => {
             </button>
           </header>
           <div className="mt-[25px] px-5">
-            <article className="prose prose-a:text-accent">{document.textContent}</article>
+            <article className="prose max-w-full prose-a:text-accent prose-p:indent-5 prose-p:text-justify">
+              {document.textContent}
+            </article>
           </div>
         </>
       )}
--- src/pages/DocumentsPage/DocumentsPage.tsx
+++ src/pages/DocumentsPage/DocumentsPage.tsx
@@ -1,25 +1,25 @@
-import { documents } from '@/textData/documentsTitles';
-import { Accordion, AccordionItem } from '@nextui-org/react';
-import { FC, useEffect } from 'react';
-import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { documents } from "@/textData/documentsTitles";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { FC, useEffect } from "react";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export const DocumentsPage: FC = () => {
   const itemClasses = {
-    base: 'w-[270px]',
-    title: 'font-bold text-[16px]',
-    heading: 'bg-[#EBF5FF] w-[270px] rounded-[13px]',
-    indicator: 'rotate-[-90deg] group-data-[open=true]:rotate-90 transition-transform',
-    trigger: 'p-[14px] group justify-between',
-    content: 'w-[256px] p-0 mt-3',
-    titleWrapper: '',
+    base: "w-[270px]",
+    title: "font-bold text-[16px]",
+    heading: "bg-[#EBF5FF] w-[270px] rounded-[13px]",
+    indicator: "rotate-[-90deg] group-data-[open=true]:rotate-90 transition-transform",
+    trigger: "p-[14px] group justify-between",
+    content: "w-[256px] p-0 mt-3",
+    titleWrapper: "",
   };
 
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (location.pathname === '/documents') {
-      navigate('oferta_na_okazanie_uslug_dlya_ispolnitelya');
+    if (location.pathname === "/documents") {
+      navigate("oferta_na_okazanie_uslug_dlya_ispolnitelya");
     }
   }, [location.pathname]);
 
@@ -28,7 +28,7 @@ export const DocumentsPage: FC = () => {
       <div className="flex gap-5">
         <nav className="px-5 py-6 rounded-[30px] bg-white shadow-md w-[310px]">
           <Accordion
-            defaultExpandedKeys={['0', '1']}
+            defaultExpandedKeys={["0", "1"]}
             itemClasses={itemClasses}
             className="p-0 gap-6"
             selectionMode="multiple"
@@ -41,9 +41,9 @@ export const DocumentsPage: FC = () => {
                     <Link
                       to={item.link}
                       className={`pl-[14px] border-l-2 mb-2 text-[12px] ${
-                        location.pathname.split('/')[2] === item.link
-                          ? 'font-bold border-solid border-black'
-                          : 'border-transparent'
+                        location.pathname.split("/")[2] === item.link
+                          ? "font-bold border-solid border-black"
+                          : "border-transparent"
                       }`}
                       key={i}
                     >
