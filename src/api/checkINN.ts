e80506e1a138d081641c866fb4ba48f92ed99390
a.nemtsov <a.nemtsov@kefirgames.com>
2025-05-07 22:08:45 +0400

    chore: upgrade framer moution version

--- src/api/checkINN.ts
+++ src/api/checkINN.ts
@@ -2,7 +2,6 @@ import { axiosInstance } from "@/utils";
 
 export const checkINN = async (inn: string) => {
   const res = await axiosInstance.get(`/v1/suggest/company?query=${inn}`);
-  console.log(!!res.data.length);
   if (res.data.length > 0) {
     return res.data[0].name as string;
   }
